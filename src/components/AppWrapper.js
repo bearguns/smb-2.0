41de649542fb31d150ef565a7bed7f8ab80e3d85
Sean Brage <seanmbrage@me.com>
2020-03-27 09:36:09 -0600

    fixes background position on mobile safari

--- src/components/AppWrapper.js
+++ src/components/AppWrapper.js
@@ -3,13 +3,21 @@ import { Context } from "./Store.js";
 import styled, { createGlobalStyle, ThemeProvider } from "styled-components";
 
 const Wrapper = styled.div`
+  padding-top: 90px;
+`;
+
+const Background = styled.div`
+  position: fixed;
+  z-index: -1;
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
   background: ${props => props.theme.background};
   background-color: ${props => props.theme.colors.pattern};
-  background-attachment: fixed;
   height: 100%;
   transition: background-color 0.25s ease-in-out;
   width: 100%;
-  padding-top: 90px;
 `;
 
 const GlobalStyle = createGlobalStyle`
@@ -46,6 +54,7 @@ export default function AppWrapper(props) {
     <React.Fragment>
       <GlobalStyle />
       <ThemeProvider theme={activeTheme}>
+	<Background />
 	<Wrapper>{children}</Wrapper>
       </ThemeProvider>
     </React.Fragment>
