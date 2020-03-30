742249bd7395ae3dd2e8b99f4aac3bd3a64b9f86
Sean Brage <seanmbrage@me.com>
2020-03-30 16:17:50 -0600

    fix page not returning to top on route change (mobile)

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,31 @@
-import React from "react";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import React, { useEffect } from "react";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  useLocation
+} from "react-router-dom";
 import Store from "./Store.js";
 import Navbar from "./navbar/Navbar.js";
 import AppWrapper from "./AppWrapper.js";
 import Home from "./pages/Home.js";
 import Portfolio from "./pages/Portfolio.js";
 import Contact from "./pages/Contact.js";
+
+const ScrollToTop = () => {
+  const { pathname } = useLocation();
+
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [pathname]);
+
+  return null;
+};
+
 function App() {
   return (
     <Router>
+      <ScrollToTop />
       <Store>
 	<AppWrapper>
 	  <Navbar />
